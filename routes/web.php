87462e223f552df3c0b14c351393d459c053c0ce
DescaMr <drizal07@gmail.com>
2023-12-05 23:32:38 +0700

    bab 6 laravel data management

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
+
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
@@ -27,6 +29,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::view('/roles', 'role')->name('role')->middleware(['role:pustakawan']);
+    Route::get('/books', [BookController::class, 'index'])->name('book');
+    Route::get('/books/create', [BookController::class, 'create'])->name('book.create');
+    Route::post('/books', [BookController::class, 'store'])->name('book.store');
+    Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('book.edit');
+    Route::match(['put', 'patch'], '/books/{id}', [BookController::class, 'update'])->name('book.update');
+    Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('book.destroy');
 });
 
 require __DIR__.'/auth.php';
